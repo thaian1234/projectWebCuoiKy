41f53d5ee2009277dc48170818a9d2c8bd517d1b
thaian1234 <121446197+thaian1234@users.noreply.github.com>
2023-11-24 20:30:17 +0700

    Thêm bootstrap

--- src/main/java/hcmute/com/projectcuoiky/Config/WebMvcConfig.java
+++ src/main/java/hcmute/com/projectcuoiky/Config/WebMvcConfig.java
@@ -2,7 +2,30 @@ package hcmute.com.projectcuoiky.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Configuration
 public class WebMvcConfig {
+    public static void saveFile(String uploadDir, String fileName,
+                                MultipartFile multipartFile) throws IOException {
+        Path uploadPath = Paths.get(uploadDir);
+
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve(fileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ioe) {
+            throw new IOException("Could not save image file: " + fileName, ioe);
+        }
+    }
 }
